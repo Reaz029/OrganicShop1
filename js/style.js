89e655b394d13959b470bdb7882d557d861acaf6
Reaz Ahmed <reaz63029@gmail.com>
2021-10-24 00:21:35 +0600

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,12 @@
+wow = new WOW(
+    {
+        boxClass: 'wow',      // default
+        animateClass: 'animated', // default
+        offset: 100,          // default
+        mobile: true,       // default
+        live: true        // default
+    }
+)
+
+new WOW().init();
+
